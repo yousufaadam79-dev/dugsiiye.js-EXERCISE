73029b0d328aa08f253b977de092413257146113
yousufaadam79-dev <yousufaadam79@gmail.com>
2026-07-14 10:56:40 +0300

    ku dar exercise-2

---
+++ Exercise-2/main.js
@@ -0,0 +1,23 @@
+A  = 15;
+B =  "15";
+
+
+// EQUAL
+console.log(A == B)
+// NOT EQUAL
+console.log(A != B)
+// SRICK NOT EQUAL
+console.log( A !== B)
+// DATA TAB
+console.log(A === B)
+// GRETER THAN
+console.log(A > B)
+// GRETER OR EQUAL
+console.log(A >= B)
+// LES THAN
+console.log(A < B)
+// LES THAN OR EQUAL
+console.log(A <= B)
+
+
+
