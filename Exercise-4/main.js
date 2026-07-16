9af737679b811b674c04cbef64e7d645e2e2c45f
yousufaadam79-dev <yousufaadam79@gmail.com>
2026-07-16 17:53:01 +0300

    Exercise-4 ayn kusoo daray

---
+++ Exercise-4/main.js
@@ -0,0 +1,22 @@
+function add( a,b) {
+    // console.log(a+b);
+    return a+b
+    
+    
+}
+console.log(add(10,5));
+console.log(add(50,5));
+console.log(add(9,5));
+ 
+
+
+// Function Expression
+
+const great = function (name) {
+    console.log("Hello " + name );
+       
+}
+
+great("mushtaaq")
+great("yuusuf")
+great("saciid")
