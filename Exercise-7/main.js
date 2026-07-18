344efba45953d5514c7903fa02585cac8c5c725a
yousufaadam79-dev <yousufaadam79@gmail.com>
2026-07-18 10:52:20 +0300

    waxaan kusoo daray Exercise-7

---
+++ Exercise-7/main.js
@@ -0,0 +1,24 @@
+let name = ["HTML" , "CSS",   "JAVA SCRIPT"]
+console.log(name);
+
+// push
+
+name.push("react")
+console.log(name);
+
+// pop
+name.pop()
+
+console.log(name);
+
+// unshift
+name.unshift("Bigner")
+console.log(name);
+
+// shift
+
+name.shift()
+console.log(name);
+
+
+
