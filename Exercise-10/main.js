e3e086c0170dba2fe48bc5219d0c2b9817e98699
yousufaadam79-dev <yousufaadam79@gmail.com>
2026-07-19 10:53:55 +0300

    Exercise-10

---
+++ Exercise-10/main.js
@@ -0,0 +1,25 @@
+let library =[
+    {
+        title: "HOGAAMINTA SUUBAN",
+        authore:"by Dr Cali Sheekh",
+        year:2009,
+    },
+    {
+        title: "HA WEYNAAN AABE",
+        authore:"AADAN SAHAN",
+        year:2024,
+    },
+    {
+        title: "CASRIGA DAANYEERTA",
+        authore:"AADAN SAHAN",
+        year:2025,
+    },
+
+
+]
+
+console.log(library[0] .title);
+console.log(library[1] .authore);
+console.log(library[1] .year);
+
+
