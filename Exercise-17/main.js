3dd87e350703e728b041b0af71ae13b2cdf1ccc0
yousufaadam79-dev <yousufaadam79@gmail.com>
2026-07-25 09:45:17 +0300

    Exercise-17

---
+++ Exercise-17/main.js
@@ -0,0 +1,16 @@
+let Temperature = 25;
+if (Temperature <0) {
+    console.log(" verry cool");
+}
+else if (Temperature <= 15 ){
+    console.log("cold");
+}
+else if (Temperature >= 15 ){
+    console.log("warm");
+}
+else{
+console.log("hott");
+
+};
+
+    
