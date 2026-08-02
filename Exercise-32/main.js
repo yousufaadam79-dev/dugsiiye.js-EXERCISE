75d62fa976c0f6f2392e22a1743448cac1afa2a5
yousufaadam79-dev <yousufaadam79@gmail.com>
2026-08-02 11:04:31 +0300

    Exercise-32 ayn kusoo daray

---
+++ Exercise-32/main.js
@@ -0,0 +1,15 @@
+const name = document.querySelector('#name');
+console.log(name);
+
+const information = document.querySelector('#information');
+console.log(name);
+
+const pragraf  = document.querySelectorAll('.pragraf')
+
+console.log(pragraf);
+
+
+const button = document.querySelector('button')
+
+console.log(button);
+
